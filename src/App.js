d47b3c58b6cc89b2219d80aa326d21976e23f846
wesselsmit <wrsmit00@gmail.com>
2021-10-06 11:53:59 +0200

    setup prettier and eslint

--- src/App.js
+++ src/App.js
@@ -2,20 +2,29 @@ const Pet = ({ name, animal, breed }) => {
   return React.createElement('div', {}, [
     React.createElement('h2', {}, name),
     React.createElement('h3', {}, animal),
-    React.createElement('h3', {}, breed)
+    React.createElement('h3', {}, breed),
   ])
 }
 
-
 const App = () => {
-  return React.createElement('div', {},
-    [
-      React.createElement('h1', {}, 'Adopt Me!'),
-      React.createElement(Pet, { name: 'Luna', animal: 'Dog', breed: 'havanese' }),
-      React.createElement(Pet, { name: 'Pepper', animal: 'Bird', breed: 'Cockatiel' }),
-      React.createElement(Pet, { name: 'Sudo', animal: 'Dog', breed: 'Wheaten Terrier' }),
-    ]
-  )
+return React.createElement('div', {}, [
+  React.createElement('h1', {}, 'Adopt Me!'),
+    React.createElement(Pet, {
+      name: 'Luna',
+      animal: 'Dog',
+      breed: 'havanese',
+    }),
+    React.createElement(Pet, {
+      name: 'Pepper',
+      animal: 'Bird',
+      breed: 'Cockatiel',
+    }),
+    React.createElement(Pet, {
+      name: 'Sudo',
+      animal: 'Dog',
+      breed: 'Wheaten Terrier',
+    }),
+  ])
 }
 
 ReactDOM.render(React.createElement(App), document.getElementById('app'))
