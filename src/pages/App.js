badf8baf9aab75729b5c94c7a64f344dc705a112
wesselsmit <wrsmit00@gmail.com>
2021-10-08 12:05:08 +0200

    add lazy loading to Details and SearchParams

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,8 +1,11 @@
-import { useState } from 'react'
+import { useState, lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import ThemeContext from '../contexts/ThemeContext'
-import Details from './Details'
-import SearchParams from '../components/SearchParams'
+
+
+const Details = lazy(() => import('./Details'))
+const SearchParams = lazy(() => import('../components/SearchParams'))
+
 
 const App = () => {
   const theme = useState('darkblue')
@@ -10,23 +13,25 @@ const App = () => {
   return (
     <ThemeContext.Provider value={theme}>
       <div>
-        <Router>
-          <header>
-            <Link to="/">
-              <h1>Adopt me!</h1>
-            </Link>
-          </header>
+        <Suspense fallback={<h2>Loading route...</h2>}>
+          <Router>
+            <header>
+              <Link to="/">
+                <h1>Adopt me!</h1>
+              </Link>
+            </header>
 
-          <Switch>
-            <Route path="/details/:id">
-              <Details />
-            </Route>
+            <Switch>
+              <Route path="/details/:id">
+                <Details />
+              </Route>
 
-            <Route path="/">
-              <SearchParams />
-            </Route>
-          </Switch>
-        </Router>
+              <Route path="/">
+                <SearchParams />
+              </Route>
+            </Switch>
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   )
